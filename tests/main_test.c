afc3d41fec4da5fb8146f63dc9c728a5ba5f8459
Aksel Sjögren <akselsjogren@users.noreply.github.com>
2019-12-01 11:30:05 +0100

    Add tests for versioncmp

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,12 +6,19 @@
 #include "../version.h"
 
 static int test_make_version(int *, int *);
+static int test_versioncmp(int *, int *);
 
 int main(int argc, char **argv)
 {
 	int total_tests = 0, tests = 0, total_passed = 0, passed = 0;
 
 	test_make_version(&tests, &passed);
+	total_tests += tests;
+	total_passed += passed;
+
+    tests = 0;
+    passed = 0;
+	test_versioncmp(&tests, &passed);
 	total_tests += tests;
 	total_passed += passed;
 
@@ -24,9 +31,6 @@ int main(int argc, char **argv)
 static int test_make_version(int *ntests, int *passed)
 {
 	struct version *v;
-
-	*ntests = 0;
-	*passed = 0;
 
 	/* make_version() stores version string */
 	(*ntests)++;
@@ -85,6 +89,55 @@ static int test_make_version(int *ntests, int *passed)
 	else
 		printf("test %-50s: failed\n", "make_version extra tokens in input");
 	free_version(v);
+
+	return *passed == *ntests ? 1 : 0;
+}
+
+int test_versioncmp(int *ntests, int *passed)
+{
+	struct version *a, *b;
+
+	/* versioncmp 1.0.0 == 1*/
+	(*ntests)++;
+	a = make_version("1.0.0", 10);
+	b = make_version("1", 10);
+    if (versioncmp(a, b) == 0)
+        (*passed)++;
+    else
+		printf("test %-50s: failed\n", "versioncmp 1.0.0 == 1");
+	free_version(a);
+	free_version(b);
+
+	/* versioncmp 1.0.0 > 0.9.1 */
+	(*ntests)++;
+	a = make_version("1.0.0", 10);
+	b = make_version("0.9.1", 10);
+    if (versioncmp(a, b) == 1)
+        (*passed)++;
+    else
+		printf("test %-50s: failed\n", "versioncmp 1.0.0 > 0.9.1");
+	free_version(a);
+	free_version(b);
+
+	/* versioncmp 9.123.4567 < 10.0.1 */
+	(*ntests)++;
+	a = make_version("9.123.4567", 0);
+	b = make_version("10.0.1", 0);
+    if (versioncmp(a, b) == -1)
+        (*passed)++;
+    else
+		printf("test %-50s: failed\n", "versioncmp 9.123.4567 < 10.0.1");
+	free_version(a);
+	free_version(b);
+
+	/* versioncmp same version object equals */
+	(*ntests)++;
+	a = make_version("5.1", 0);
+    if (versioncmp(a, a) == 0)
+        (*passed)++;
+    else
+		printf("test %-50s: failed\n", "versioncmp 9.123.4567 < 10.0.1");
+	free_version(a);
 
 	return *passed == *ntests ? 1 : 0;
 }
